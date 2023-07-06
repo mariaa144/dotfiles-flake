a5bd3e09f594801b9c9ce47230f5fa16632d0e6e
Yuchen Guo <yguo@posteo.net>
2023-07-06 08:29:42 +0200

    add support for per-host drop-in config
    Signed-off-by: Yuchen Guo <yguo@posteo.net>

--- configuration.nix
+++ configuration.nix
@@ -1,16 +1,4 @@
-{ my-config, zfs-root, inputs, pkgs, lib, ... }: {
-  # load module config to top-level configuration
-  inherit my-config zfs-root;
-
-  # Let 'nixos-version --json' know about the Git revision
-  # of this flake.
-  system.configurationRevision = if (inputs.self ? rev) then
-    inputs.self.rev
-  else
-    throw "refuse to build: git tree is dirty";
-
-  system.stateVersion = "22.11";
-
+{ ... }: {
   # Enable NetworkManager for wireless networking,
   # You can configure networking with "nmtui" command.
   networking.useDHCP = true;
@@ -28,11 +16,6 @@
     viAlias = true;
     vimAlias = true;
   };
-
-  imports = [
-    "${inputs.nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
-    # "${inputs.nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
-  ];
 
   services.openssh = {
     enable = lib.mkDefault true;
--- flake.nix
+++ flake.nix
@@ -1,27 +1,64 @@
 {
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  ## after reboot, you can track rolling release by using
-  #inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
-  outputs = { self, nixpkgs }@inputs:
+  outputs = { self, nixpkgs, home-manager }:
     let
-      lib = nixpkgs.lib;
-      mkHost = { my-config, zfs-root, pkgs, system, ... }:
-        lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./modules
-            (import ./configuration.nix {
-              inherit my-config zfs-root inputs pkgs lib;
-            })
-          ];
-        };
+      mkHost = hostName: system:
+        (({ my-config, zfs-root, pkgs, system, ... }:
+          nixpkgs.lib.nixosSystem {
+            inherit system;
+            modules = [
+              # Module 0: zfs-root and my-config
+              ./modules
+
+              # Module 1: host-specific config, if exist
+              (if (builtins.pathExists
+                ./hosts/${hostName}/configuration.nix) then
+                import ./hosts/${hostName}/configuration.nix
+              else
+                { })
+
+              # Module 2: entry point
+              (({ my-config, zfs-root, pkgs, lib, ... }: {
+                inherit my-config zfs-root;
+                system.configurationRevision = if (self ? rev) then
+                  self.rev
+                else
+                  throw "refuse to build: git tree is dirty";
+                system.stateVersion = "23.05";
+                imports = [
+                  "${nixpkgs}/nixos/modules/installer/scan/not-detected.nix"
+                  # "${nixpkgs}/nixos/modules/profiles/hardened.nix"
+                  # "${nixpkgs}/nixos/modules/profiles/qemu-guest.nix"
+                ];
+              }) {
+                inherit my-config zfs-root pkgs;
+                lib = nixpkgs.lib;
+              })
+
+              # Module 3: home-manager
+              home-manager.nixosModules.home-manager
+              {
+                home-manager.useGlobalPkgs = true;
+                home-manager.useUserPackages = true;
+              }
+
+              # Module 4: config shared by all hosts
+              (import ./configuration.nix)
+            ];
+          }) (import ./hosts/${hostName} {
+            system = system;
+            pkgs = nixpkgs.legacyPackages.${system};
+          }));
     in {
       nixosConfigurations = {
-        exampleHost = let
-          system = "x86_64-linux";
-          pkgs = nixpkgs.legacyPackages.${system};
-        in mkHost (import ./hosts/exampleHost { inherit system pkgs; });
+        exampleHost = mkHost "exampleHost" "x86_64-linux";
       };
     };
 }
