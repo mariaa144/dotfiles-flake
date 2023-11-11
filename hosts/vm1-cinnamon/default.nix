6f245efa5edbebd9e5b463eeea87f28ea8c0b9f4
sdelrio <oftheriver@gmail.com>
2023-11-11 14:23:39 +0100

    vm1-cinnamom: add nerdfonts

--- hosts/vm1-cinnamon/default.nix
+++ hosts/vm1-cinnamon/default.nix
@@ -120,4 +120,17 @@
     zsh
   ];
 
+  # https://github.com/ryanoasis/nerd-fonts/tree/master/patched-fonts/
+  # https://nixos.wiki/wiki/Fonts
+  fonts.fonts = with pkgs; [
+    (nerdfonts.override {
+      fonts = [ 
+       "FiraCode"
+       "DroidSansMono"
+#       "DejaVuSansMono"
+#       "SourceCodePro"
+      ];
+    })
+  ];
+
 }
